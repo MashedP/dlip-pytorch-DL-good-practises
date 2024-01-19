228cc32700b6c2ceb7e81e6eb5e5e2d742e507f0
MashdedP <cyrousselot@gmail.com>
2024-01-19 16:38:03 +0100

    fix(Structure & Documentation): :bug: Consistency of split

--- src/dlip/data/data.py
+++ src/dlip/data/data.py
@@ -2,11 +2,13 @@ import torchvision
 from torch.utils.data import random_split
 from torchvision import transforms
 from dlip.data.usps import download_usps
-
+import torch
 def load_dataset(path):
+    generator = torch.Generator()
+    generator.manual_seed(0)
     download_usps(path)
     dataset = torchvision.datasets.USPS(
         root=path + "USPS/", train=True, transform=transforms.ToTensor(), download=False
     )
-    train_set, val_set = random_split(dataset, [6000, 1291])
+    train_set, val_set = random_split(dataset, [6000, 1291],generator=generator)
     return train_set, val_set
